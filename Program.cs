dbda73d095c78861a8c8db9190e332e03129ec02
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-10-03 15:21:14 -0300

    Enable Swagger in all environments
    Swagger and SwaggerUI are now enabled regardless of the environment, allowing API documentation and testing in both development and production.

--- Program.cs
+++ Program.cs
@@ -29,12 +29,10 @@ builder.Services.AddAutoMapper(cfg =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
