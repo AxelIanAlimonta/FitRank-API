f994b542e9b831e0011c344a354ed7e1540eaa0f
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-10-31 16:48:49 -0300

    Add AdministradorId to Gimnasio DTOs
    Added the AdministradorId property to AgregarGimnasioDTO, ActualizarGimnasioDTO, and ObtenerGimnasioDTO to support tracking the administrator associated with a gym. Also added Id property to ActualizarGimnasioDTO for update operations.

--- Application/DTOs/GimnasioDTOs/ActualizarGimnasioDTO.cs
+++ Application/DTOs/GimnasioDTOs/ActualizarGimnasioDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ActualizarGimnasioDTO
     {
+        public int Id { get; set; }
         public string? Nombre { get; set; }
         public string? Direccion { get; set; }
         public string? RazonSocial { get; set; }
@@ -11,5 +12,6 @@
         public string? Email { get; set; }
         public string? Telefono { get; set; }
         public string? Cuil { get; set; }
+        public long? AdministradorId { get; set; }
     }
 }
