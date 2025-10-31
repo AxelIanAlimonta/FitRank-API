bb41227916ebab70cb082b8874dc3b66ee1c5b33
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-10-31 16:23:38 -0300

    Add unit tests for Entrenamiento use cases
    Added test classes for Agregar, Actualizar, Eliminar, ObtenerPorId, and ObtenerTodos use cases of Entrenamiento. These tests cover scenarios for valid data, non-existent entities, and empty results, improving coverage and reliability of the Entrenamiento application logic.

---
+++ tests/CasosDeUsoTests/EntrenamientoCasosDeUsoTests/EliminarEntrenamientoCasoDeUsoTests.cs
@@ -0,0 +1,61 @@
+﻿using FitRank_API.Infrastructure.Interfaces;
+using Moq;
+using AutoMapper;
+using Xunit;
+using FitRank_API.Domain.Entities;
+using FluentAssertions;
+using FitRank_API.Application.MappingProfiles;
+using FitRank_API.Application.DTOs.EntrenamientoDTOs;
+using FitRank_API.Application.UseCases.Entrenamiento;
+
+namespace CasosDeUsoTests.EntrenamientoCasosDeUsoTests;
+
+public class EliminarEntrenamientoCasoDeUsoTests
+{
+    private readonly Mock<IEntrenamientoRepositorio> _entrenamientoRepositorioMock;
+
+    public EliminarEntrenamientoCasoDeUsoTests()
+    {
+
+        _entrenamientoRepositorioMock = new Mock<IEntrenamientoRepositorio>();
+    }
+
+    [Fact]
+    public async Task EliminarEntrenamiento_CuandoElEntrenamientoExiste_RetornaTrue()
+    {
+        // Arrange
+        long entrenamientoId = 1;
+
+        _entrenamientoRepositorioMock
+            .Setup(repo => repo.EliminarAsync(entrenamientoId))
+            .ReturnsAsync(true);
+
+        var casoDeUso = new EliminarEntrenamientoCasoDeUso(_entrenamientoRepositorioMock.Object);
+
+        // Act
+        var resultado = await casoDeUso.Ejecutar(entrenamientoId);
+
+        // Assert
+        resultado.Should().BeTrue();
+
+    }
+
+    [Fact]
+    public async Task EliminarEntrenamiento_CuandoElEntrenamientoNoExiste_RetornaFalse()
+    {
+        // Arrange
+        long entrenamientoId = 99; // Suponiendo que este ID no existe
+
+        _entrenamientoRepositorioMock
+            .Setup(repo => repo.EliminarAsync(entrenamientoId))
+            .ReturnsAsync(false);
+
+        var casoDeUso = new EliminarEntrenamientoCasoDeUso(_entrenamientoRepositorioMock.Object);
+
+        // Act
+        var resultado = await casoDeUso.Ejecutar(entrenamientoId);
+
+        // Assert
+        resultado.Should().BeFalse();
+    }
+}
