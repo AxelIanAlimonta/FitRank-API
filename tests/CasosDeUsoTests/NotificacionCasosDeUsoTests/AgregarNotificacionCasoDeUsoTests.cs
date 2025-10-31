b303f2829e3b7b4084995b1adab1ce6f78ab6f77
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-10-31 18:43:03 -0300

    Add unit tests for Notificacion use cases
    Added tests for AgregarNotificacionCasoDeUso and ObtenerNotificacionPorUsuarioCasoDeUso to verify correct behavior when adding notifications and retrieving notifications by user.

---
+++ tests/CasosDeUsoTests/NotificacionCasosDeUsoTests/AgregarNotificacionCasoDeUsoTests.cs
@@ -0,0 +1,89 @@
+﻿using FitRank_API.Infrastructure.Interfaces;
+using Moq;
+using AutoMapper;
+using Xunit;
+using FitRank_API.Domain.Entities;
+using FluentAssertions;
+using FitRank_API.Application.MappingProfiles;
+using FitRank_API.Application.DTOs.NotificacionDTOs;
+using FitRank_API.Application.CasosDeUso.NotificacionCasosDeUso;
+using FitRank_API.Application.Mappings;
+
+namespace CasosDeUsoTests.NotificacionCasosDeUsoTests;
+
+public class AgregarNotificacionCasoDeUsoTests
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<INotificacionRepositorio> _notificacionRepositorioMock;
+
+    public AgregarNotificacionCasoDeUsoTests()
+    {
+        var mappingConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new NotificacionProfile());
+        });
+        _mapper = mappingConfig.CreateMapper();
+        _notificacionRepositorioMock = new Mock<INotificacionRepositorio>();
+    }
+
+    [Fact]
+    public async Task AgregarNotificacion_CuandoLosDatosSonValidos_RetornaNotificacionDTO()
+    {
+        // Arrange
+        var nuevaNotificacionDTO = new AgregarNotificacionDTO
+        {
+            Id = 0,
+            Mensaje = "Nueva notificación",
+            Titulo = "Notificación",
+            UsuarioEmisorId = 1,
+            UsuarioReceptorId = 2,
+            Activa = true,
+            Leido = false,
+            FechaEnvio = new DateTime(2024, 6, 1)
+        };
+
+        var notificacionAGuardar = new Notificacion
+        {
+            Id = 0,
+            Mensaje = "Nueva notificación",
+            Titulo = "Notificación",
+            UsuarioEmisorId = 1,
+            UsuarioReceptorId = 2,
+            Activa = true,
+            Leido = false,
+            FechaEnvio = new DateTime(2024, 6, 1)
+        };
+
+        var notificacionGuardada = new Notificacion
+        {
+            Id = 0,
+            Mensaje = "Nueva notificación",
+            Titulo = "Notificación",
+            UsuarioEmisorId = 1,
+            UsuarioReceptorId = 2,
+            Activa = true,
+            Leido = false,
+            FechaEnvio = new DateTime(2024, 6, 1)
+        };
+
+        _notificacionRepositorioMock
+            .Setup(repo => repo.AgregarAsync(It.IsAny<Notificacion>()))
+            .ReturnsAsync(notificacionGuardada);
+
+        var agregarNotificacionCasoDeUso = new AgregarNotificacionCasoDeUso(_notificacionRepositorioMock.Object, _mapper);
+
+        // Act
+        var resultado = await agregarNotificacionCasoDeUso.Ejecutar(nuevaNotificacionDTO);
+
+        // Assert
+        resultado.Should().NotBeNull();
+        resultado.Id.Should().Be(notificacionGuardada.Id);
+        resultado.Mensaje.Should().Be(nuevaNotificacionDTO.Mensaje);
+        resultado.Titulo.Should().Be(nuevaNotificacionDTO.Titulo);
+        resultado.UsuarioEmisorId.Should().Be(nuevaNotificacionDTO.UsuarioEmisorId);
+        resultado.UsuarioReceptorId.Should().Be(nuevaNotificacionDTO.UsuarioReceptorId);
+        resultado.FechaEnvio.Should().Be(nuevaNotificacionDTO.FechaEnvio);
+        resultado.Activa.Should().Be(nuevaNotificacionDTO.Activa);
+        resultado.Leido.Should().Be(nuevaNotificacionDTO.Leido);
+    }
+}
---
+++ tests/CasosDeUsoTests/NotificacionCasosDeUsoTests/ObtenerNotificacionPorUsuarioCasoDeUsoTests.cs
@@ -0,0 +1,96 @@
+﻿using FitRank_API.Infrastructure.Interfaces;
+using Moq;
+using AutoMapper;
+using Xunit;
+using FitRank_API.Domain.Entities;
+using FluentAssertions;
+using FitRank_API.Application.MappingProfiles;
+using FitRank_API.Application.DTOs.NotificacionDTOs;
+using FitRank_API.Application.CasosDeUso.NotificacionCasosDeUso;
+using FitRank_API.Application.Mappings;
+
+namespace CasosDeUsoTests.NotificacionCasosDeUsoTests;
+
+public class ObtenerNotificacionPorUsuarioCasoDeUsoTests
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<INotificacionRepositorio> _notificacionRepositorioMock;
+
+    public ObtenerNotificacionPorUsuarioCasoDeUsoTests()
+    {
+        var mappingConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new NotificacionProfile());
+        });
+        _mapper = mappingConfig.CreateMapper();
+        _notificacionRepositorioMock = new Mock<INotificacionRepositorio>();
+    }
+
+    [Fact]
+    public async Task ObtenerNotificacionesPorUsuario_CuandoElUsuarioTieneNotificaciones_RetornaListaDeNotificacionDTO()
+    {
+        // Arrange
+        long usuarioReceptorId = 2;
+
+        var notificacionesEnRepositorio = new List<Notificacion>
+        {
+            new Notificacion
+            {
+                Id = 1,
+                Mensaje = "Notificación 1",
+                Titulo = "Título 1",
+                UsuarioEmisorId = 1,
+                UsuarioReceptorId = usuarioReceptorId,
+                Activa = true,
+                Leido = false,
+                FechaEnvio = new DateTime(2024, 6, 1)
+            },
+            new Notificacion
+            {
+                Id = 2,
+                Mensaje = "Notificación 2",
+                Titulo = "Título 2",
+                UsuarioEmisorId = 3,
+                UsuarioReceptorId = usuarioReceptorId,
+                Activa = true,
+                Leido = true,
+                FechaEnvio = new DateTime(2024, 6, 2)
+            }
+        };
+
+        _notificacionRepositorioMock
+            .Setup(repo => repo.ObtenerPorUsuarioAsync(usuarioReceptorId))
+            .ReturnsAsync(notificacionesEnRepositorio);
+
+        var casoDeUso = new ObtenerNotificacionPorUsuarioCasoDeUso(_notificacionRepositorioMock.Object, _mapper);
+
+        // Act
+        var resultado = await casoDeUso.Ejecutar(usuarioReceptorId);
+
+        // Assert
+        resultado.Should().NotBeNull();
+        resultado.Count().Should().Be(notificacionesEnRepositorio.Count);
+        resultado.Should().BeEquivalentTo(_mapper.Map<IEnumerable<ObtenerNotificacionDTO>>(notificacionesEnRepositorio));
+
+    }
+
+    [Fact]
+    public async Task ObtenerNotificacionesPorUsuario_CuandoElUsuarioNoTieneNotificaciones_RetornaListaVacia()
+    {
+        // Arrange
+        long usuarioReceptorId = 99;
+
+        _notificacionRepositorioMock
+            .Setup(repo => repo.ObtenerPorUsuarioAsync(usuarioReceptorId))
+            .ReturnsAsync(new List<Notificacion>());
+
+        var casoDeUso = new ObtenerNotificacionPorUsuarioCasoDeUso(_notificacionRepositorioMock.Object, _mapper);
+
+        // Act
+        var resultado = await casoDeUso.Ejecutar(usuarioReceptorId);
+
+        // Assert
+        resultado.Should().NotBeNull();
+        resultado.Should().BeEmpty();
+    }
+}
