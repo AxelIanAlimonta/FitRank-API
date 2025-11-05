9cd4928ac7c64d22ed9eeb43583ad2ffdf2568ed
Jero <jeronimo1080@hotmail.com>
2025-11-05 01:38:30 -0300

    Se agregan validaciones de ranking para socios

---
+++ Application/DTOs/PuntajeDTOs/SocioRankingDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FitRank_API.Application.DTOs.PuntajeDTOs
+{
+    public class SocioRankingDto
+    {
+        public long SocioId { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public double PuntajeTotal { get; set; }
+    }
+}
