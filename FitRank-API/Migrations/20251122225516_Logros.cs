a4309f311472cd44de2b2c7fdb046280701f41be
Agustín <ag.n.delgado@gmail.com>
2025-11-24 15:19:38 -0300

    Migracion nueva para Logros

--- FitRank-API/Migrations/20251122225516_Logros.cs
+++ FitRank-API/Migrations/20251122225516_Logros.cs
@@ -10,13 +10,63 @@ namespace FitRank_API.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateIndex(
+                name: "IX_LogroSocio_GimnasioId",
+                table: "LogroSocio",
+                column: "GimnasioId");
 
+            migrationBuilder.CreateIndex(
+                name: "IX_LogroSocio_SocioId",
+                table: "LogroSocio",
+                column: "SocioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LogrosGimnasio_Gimnasios_GimnasioId",
+                table: "LogrosGimnasio",
+                column: "GimnasioId",
+                principalTable: "Gimnasios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LogroSocio_Gimnasios_GimnasioId",
+                table: "LogroSocio",
+                column: "GimnasioId",
+                principalTable: "Gimnasios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LogroSocio_Socios_SocioId",
+                table: "LogroSocio",
+                column: "SocioId",
+                principalTable: "Socios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LogrosGimnasio_Gimnasios_GimnasioId",
+                table: "LogrosGimnasio");
 
+            migrationBuilder.DropForeignKey(
+                name: "FK_LogroSocio_Gimnasios_GimnasioId",
+                table: "LogroSocio");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LogroSocio_Socios_SocioId",
+                table: "LogroSocio");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LogroSocio_GimnasioId",
+                table: "LogroSocio");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LogroSocio_SocioId",
+                table: "LogroSocio");
         }
     }
 }
