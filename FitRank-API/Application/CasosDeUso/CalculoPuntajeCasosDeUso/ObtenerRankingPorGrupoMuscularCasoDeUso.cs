23ad994be4aecd7fdc88796c0d36acf6904fb36e
LAUTYslz <salazarlautaroalejandro@hotmail.com>
2025-11-25 14:48:36 -0300

    Actualizacion de BDD, nuevas migracion Todo final.

--- FitRank-API/Application/CasosDeUso/CalculoPuntajeCasosDeUso/ObtenerRankingPorGrupoMuscularCasoDeUso.cs
+++ FitRank-API/Application/CasosDeUso/CalculoPuntajeCasosDeUso/ObtenerRankingPorGrupoMuscularCasoDeUso.cs
@@ -19,7 +19,7 @@ namespace FitRank_API.Application.CasosDeUso.CalculoPuntajeCasosDeUso
             _grupoMuscularRepositorio = grupoMuscularRepositorio;
         }
 
-        public virtual async Task<List<ObtenerRankingPorGrupoMuscularDTO>> Ejecutar(long gimnasioId, string grupo, int cantidad)
+        public virtual async Task<List<ObtenerRankingPorGrupoMuscularDTO>> Ejecutar(long gimnasioId, long grupoId, int cantidad)
         {
             var grupoMuscular = await _grupoMuscularRepositorio.ObtenerPorIdAsync(grupoId);
             if (grupoMuscular == null)
