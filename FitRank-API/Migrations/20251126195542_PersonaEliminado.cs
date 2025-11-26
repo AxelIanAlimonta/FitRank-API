296e7b8c6e371b2147a981f10737a1ff113076b6
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-11-26 16:56:26 -0300

    Eliminar entidad `Persona` del modelo y la base de datos
    Se ha eliminado la entidad `Persona` del modelo de datos y de la base
    de datos. Esto incluye:
    
    - Eliminación de la tabla `Personas` en la migración
      `20251126195542_PersonaEliminado`.
    - Definición de la lógica en el método `Down` para recrear la tabla
      en caso de revertir la migración.
    - Actualización del archivo `20251126195542_PersonaEliminado.Designer.cs`
      para reflejar la eliminación de la entidad `Persona`.
    - Eliminación de la configuración de la entidad `Persona` en
      `FitRankDbContextModelSnapshot.cs` para que no sea parte del modelo
      persistente.
    
    Estos cambios aseguran que la entidad `Persona` ya no esté presente
    en el esquema actual ni en el modelo de datos.

---
+++ FitRank-API/Migrations/20251126195542_PersonaEliminado.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FitRank_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersonaEliminado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Edad = table.Column<int>(type: "integer", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+        }
+    }
+}
